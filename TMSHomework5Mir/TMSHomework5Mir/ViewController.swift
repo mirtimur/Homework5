af95d5413067a0519ccf6dd41ca0b5961c5c3aad
Mir TImur <mirtimur1@gmail.com>
2023-12-11 12:14:16 +0300

    Homework

--- TMSHomework5Mir/TMSHomework5Mir/ViewController.swift
+++ TMSHomework5Mir/TMSHomework5Mir/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  TMSHomework5Mir
-//
-//  Created by Timur Mir on 11.12.23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var array = [(1, "x"), (4, "y"), (6, "a"), (9, "u"), (5, "k")]
+        
+        //MARK: - Factorial calculation
+        var intArray = [1,2,3,4,5,6]
+        print(getFactorial(array: intArray))
+        
+        //MARK: - Squared tuple array
+        let squared = array.map { pow(Decimal($0.0),2) }
+        print(squared)
+        
+        //MARK: - Even number filtering
+        let filtering = array.filter { $0.0 % 2 == 0 }.map { $0.0 }
+        print(filtering)
+        
+        //MARK: - Filtering strings by alphabetical order
+        let stringsFiltering = array.map { $0.1 }.sorted()
+        print(stringsFiltering)
+        
     }
-
-
+    
+    func factorial(_ x: Int) -> Int {
+        if x <= 1 {
+            return 1
+        }
+        return x * factorial(x-1)
+    }
+    
+    func getFactorial(array:[Int]) -> Array<Int> {
+        return array.map { number in
+            return Int(factorial(number))
+ }
+    }
+    
 }
-
